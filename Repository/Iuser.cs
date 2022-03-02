714477c47d4c497af0a53f4dd27f8059709d34e3
Mohamed Osama <73117933+mohamedosamaeg@users.noreply.github.com>
2022-03-03 04:40:58 +0500

    004

---
+++ Repository/Iuser.cs
@@ -0,0 +1,17 @@
+﻿using facebook.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace facebook.Repository
+{
+    public interface Iuser:IGenericCRUD<User>
+    {
+        bool IsPasswordMatch(string Password, string ConfirmPassword);
+        User LogIn(string UserName, string Password);
+        bool IsUserValidate(User entity, bool IsUpdate = false);
+        bool IsUserNameExist(string UserName);
+        bool IsExist(int Id);
+        bool IsExist(string UserName, string Password);
+    }
+}
